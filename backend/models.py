7dedbb16c0b2ed9707adb4974b665ff15b3f685f
DevDepre <nycolasdasilvalucchesi@gmail.com>
2025-07-24 03:57:08 -0300

    Modificacao no models.py
    Nesse commit houve uma alteracao na maneira como
    escolhemos o tipo do resgistro(ganho ou gasto), antes
    foi feito com Enum, agora e utilizado um Varchar como valores
    pre definidos.

--- backend/models.py
+++ backend/models.py
@@ -1,31 +1,26 @@
 from sqlalchemy import Column, ForeignKey
-from sqlalchemy.types import  Integer, String, Float, Text, DateTime, Enum as SQLALCHEMYENUM
+from sqlalchemy.types import  Integer, String, Float, Text, DateTime
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from backend.database import Base
-from enum import Enum
 
-class tipoEnum(Enum):
-    GANHO = "Ganho"
-    GASTO = "Gasto"
+class Financa(Base):
+    __tablename__ = "financa"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
+    name = Column(String(100), nullable=False)
+    value = Column(Float, nullable=False)
+    type_input = Column(String(20), nullable=False)
+    created_at = Column(DateTime, default=func.now())
+    description = Column(Text, nullable=False)
+    category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
+    category = relationship("Category", back_populates="financas")
+
 
 class Category(Base):
     __tablename__ = "category"
 
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
     name = Column(String(100), nullable=False)
 
-    financas = relationship("Financas", back_populates="category", cascade="all, delete")
-
-class Financas(Base):
-    __tablename__ = "financa"
-
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=True)
-    name = Column(String(100), nullable=False)
-    value = Column(Float, nullable=False)
-    tipo = Column(Enum(tipoEnum), nullable=False)
-    created_at = Column(DateTime, default=func.now())
-    description = Column(Text, nullable=False)
-    category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
-
-    category = relationship("Category", back_populates="financas")
+    financas = relationship("Financa", back_populates="category", cascade="all, delete")
